53dfd9e8c81e3529ecfd229c60cac707d02738cd
Jolp7 <jolp791@hotmail.Fr>
2022-03-18 16:00:02 -0400

    azedaedz

--- decidim-conferences/app/models/decidim/conference_speaker_conference_meeting.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  class ConferenceSpeakerConferenceMeeting < ApplicationRecord
-    belongs_to :conference_speaker, class_name: "Decidim::ConferenceSpeaker"
-    belongs_to :conference_meeting, class_name: "Decidim::ConferenceMeeting"
-  end
-end
--- decidim-conferences/db/migrate/20210408204953_allow_null_logo_column_in_decidim_conferences_partners_table.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AllowNullLogoColumnInDecidimConferencesPartnersTable < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :decidim_conferences_partners, :logo, true
-  end
-end
